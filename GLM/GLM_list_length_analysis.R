8e90bcb24c8de19e8b2c1a1c002ec4c6f4830316
arzeilinger <arzeilinger@gmail.com>
2016-02-19 10:36:39 -0800

    Commiting first versions of museum analysis functions and list length data. Updating GLMM script.

--- GLM/GLM_list_length_analysis.R
+++ GLM/GLM_list_length_analysis.R
@@ -1,15 +1,12 @@
 #### GLMM model with list length analysis and spatial random effect
 #### Inspired by Isaac et al. 2014 and using Gio's code
 
+rm(list = ls())
 #### Preliminaries
 my_packages<-c('lme4', 'data.table', 'tidyr', 'lattice', 'dplyr')
 lapply(my_packages, require, character.only=T)
 
-# Load California polygon, object named CAsp
-#load("CA_SpatialPolygons.Rdata")
-# Load Occupancy functions
-rm(list = ls())
-## Set working directory
+## Set working directory and load Occupancy functions
 setwd("C:/Users/Adam/Documents/UC Berkeley post doc/BIGCB/Pest Project/Potato psyllid")
 source("C:/Users/Adam/Documents/GitHub/potato_psyllid_distribution_modeling/museum_specimen_analysis_functions.R")
 
@@ -28,13 +25,26 @@ hist(detectData[detectData$list_length >= 3,]$list_length, breaks = seq(3,40,1))
 hist(detectData$collectors_length) # very few lists with multiple collectors
 table(detectData$detection, detectData$list_length) # Still have a problem of declining detections with greater list length
 
+#################################################################################
+#### GLMMs
+
 # GLMM with all species lists
 glmMod <- glmer(detection ~ standardize(log(list_length)) + standardize(year) + standardize(month) + I(standardize(month)^2) + 
                   standardize(aet) + standardize(cwd) + standardize(tmn) + standardize(tmx) + (1|cellID), 
                 family = "binomial", data = detectData)              
+summary(glmMod)
 
 # GLMM with "long" species lists (length > 2)
 longListsData <- detectData[detectData$list_length >= 3,]
 glmModLL <- glmer(detection ~ standardize(log(list_length)) + standardize(year) + standardize(month) + I(standardize(month)^2) + 
                     standardize(aet) + standardize(cwd) + standardize(tmn) + standardize(tmx) + (1|cellID), 
-                  family = "binomial", data = longListsData)              
+                  family = "binomial", data = longListsData,
+                  control = glmerControl(optimizer = "bobyqa"))
+summary(glmModLL)
+
+glmModLL <- glmer(detection ~ standardize(log(list_length)) + standardize(year) +
+                    (1|cellID), 
+                  family = "binomial", data = longListsData,
+                  control = glmerControl(optimizer = "Nelder_Mead",
+                                         optCtrl = list(maxfun = 1000000)))
+summary(glmModLL)
