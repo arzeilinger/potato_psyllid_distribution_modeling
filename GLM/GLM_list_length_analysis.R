63a7e08e2d78634da1cc7261dab074a683ac958e
arzeilinger <arzeilinger@gmail.com>
2016-02-26 13:54:46 -0800

    Added a year-month interaction model; 15 km cell size looks best.

--- GLM/GLM_list_length_analysis.R
+++ GLM/GLM_list_length_analysis.R
@@ -3,7 +3,7 @@
 
 rm(list = ls())
 #### Preliminaries
-my_packages<-c('lme4', 'data.table', 'tidyr', 'lattice', 'dplyr')
+my_packages<-c('lme4', 'lmerTest', 'data.table', 'tidyr', 'lattice', 'dplyr', 'bbmle')
 lapply(my_packages, require, character.only=T)
 
 ## Set working directory and load Occupancy functions
@@ -17,73 +17,62 @@ AllLists <- readRDS("Potato psyllid data/All_Hemip_Lists_Climate_15km_Cells_2016
 ppCollectors <- readRDS("Potato psyllid data/potato_psyllid_collectors.rds")
 
 # Keep only long lists (ll > 2)
-longLists <- AllLists %>% rbindlist() %>% as.data.frame() %>% make_lists(., min.list.length = 3)
-longData <- longLists %>% rbindlist() %>% as.data.frame()
+longLists <- AllLists %>% rbindlist() %>% as.data.frame() %>% make_lists(., min.list.length = 4)
+longListsDF <- longLists %>% rbindlist() %>% as.data.frame()
 
 # lists that contain collectors of potato psyllids
-ppcCollections <- longData[longData$Collector %in% ppCollectors, "collectionID"]
-ppcData <- longData[longData$collectionID %in% ppcCollections,]
+ppcCollections <- longListsDF[longListsDF$Collector %in% ppCollectors, "collectionID"]
+ppcData <- longListsDF[longListsDF$collectionID %in% ppcCollections,]
 ppcLists <- ppcData %>% make_lists(., min.list.length = 3)
 
 # Transform to data frame with pp detection
-longListsData <- detectDataFunc(ppcLists) # Long lists
-ppLists.i <- which(longListsData$detection == 1)
+detectData <- detectDataFunc(ppcLists) # Long lists
+ppLists.i <- which(detectData$detection == 1)
 
 
-# Exploration for dynamic occupancy model
-# Split data by collecting event (year+cell)
-# repeatVisits <- split(AllData,AllData$eventID)
-# numVisits <- as.numeric(unlist(lapply(1:length(repeatVisits), function(x) length(unique(repeatVisits[[x]]$ymo)))))
-
-# Exploring the data set
-hist(detectData$collectors_length) # very few lists with multiple collectors
-table(detectData$detection, detectData$list_length) # Still have a problem of declining detections with greater list length
-# Distribution of list lengths that contain potato psyllid occurrences
+# Exploring data
+table(detectData$detection)
+with(detectData, hist(list_length, breaks = seq(min(list_length),max(list_length),1)))
 ppData <- detectData[detectData$detection == 1,]
 hist(ppData$list_length)
-# Most potato psyllid occurrences are in very short lists, with a declining number of long lists containing psyllids
 
-
-# Exploring long lists (ll >= 3)
-longListsData <- detectData[detectData$list_length >= 3,]
-hist(longListsData$list_length, breaks = seq(3,40,1))
-length(unique(longListsData$cellID)) # 858 different cells -- too many for the site random effect
-cellTable <- longListsData %>% group_by(cellID) %>% summarise(nvisits = length(cellID))
+length(unique(detectData$cellID)) # 858 different cells -- too many for the site random effect
+cellTable <- detectData %>% group_by(cellID) %>% summarise(nvisits = length(cellID))
 
 #################################################################################
 #### GLMMs
-
-# GLMM with all species lists
-glmMod <- glmer(detection ~ standardize(log(list_length)) + standardize(year) + standardize(month) + I(standardize(month)^2) + 
-                  standardize(aet) + standardize(cwd) + standardize(tmn) + standardize(tmx) + (1|cellID), 
-                family = "binomial", data = detectData)              
-summary(glmMod)
 
 # GLMM with "long" species lists (length > 2)
 # Full model
 glmModLL1 <- glmer(detection ~ standardize(log(list_length)) + standardize(year) + standardize(month) + I(standardize(month)^2) + 
                     standardize(aet) + standardize(cwd) + standardize(tmn) + standardize(tmx) + (1|cellID), 
-                  family = "binomial", data = longListsData,
-                  control = glmerControl(optimizer = "bobyqa"))
+                  family = "binomial", data = detectData,
+                  control = glmerControl(optimizer = "Nelder_Mead"))
 # Model without month
 glmModLL2 <- glmer(detection ~ standardize(log(list_length)) + standardize(year) +  
                     standardize(aet) + standardize(cwd) + standardize(tmn) + standardize(tmx) + (1|cellID), 
-                  family = "binomial", data = longListsData,
-                  control = glmerControl(optimizer = "bobyqa"))
+                  family = "binomial", data = detectData,
+                  control = glmerControl(optimizer = "Nelder_Mead"))
 # Model with only tmn and tmx
 glmModLL3 <- glmer(detection ~ standardize(log(list_length)) + standardize(year) + 
                     standardize(tmn) + standardize(tmx) + (1|cellID), 
-                  family = "binomial", data = longListsData,
+                  family = "binomial", data = detectData,
                   control = glmerControl(optimizer = "bobyqa"))
 # Model with only climate variables
 glmModLL4 <- glmer(detection ~ standardize(log(list_length)) +  
                     standardize(aet) + standardize(cwd) + standardize(tmn) + standardize(tmx) + (1|cellID), 
-                  family = "binomial", data = longListsData,
+                  family = "binomial", data = detectData,
                   control = glmerControl(optimizer = "bobyqa"))
 # Model with only year and LL
 glmModLL5 <- glmer(detection ~ standardize(log(list_length)) + standardize(year) +
                     (1|cellID), 
-                  family = "binomial", data = longListsData,
+                  family = "binomial", data = detectData,
                   control = glmerControl(optimizer = "bobyqa"))
-AIC(glmModLL1,glmModLL2,glmModLL3,glmModLL4,glmModLL5)
-summary(glmModLL4)
+# Model with year*month interaction
+glmModLL6 <- glmer(detection ~ standardize(log(list_length)) + standardize(year)*standardize(month) +
+                     standardize(year)*I(standardize(month)^2) +
+                    (1|cellID), 
+                  family = "binomial", data = detectData,
+                  control = glmerControl(optimizer = "bobyqa"))
+AICtab(glmModLL1,glmModLL2,glmModLL3,glmModLL4,glmModLL5,glmModLL6, base = TRUE)
+summary(glmModLL1)
