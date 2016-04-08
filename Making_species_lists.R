063be2c2c5a70895a17331fa24c21987cb603411
arzeilinger <arzeilinger@gmail.com>
2016-04-07 21:17:30 -0700

    Major re-organization of data and analysis scripts.

--- Making_species_lists.R
+++ Making_species_lists.R
@@ -2,7 +2,7 @@
 ##### SETTING THINGS UP #####
 rm(list = ls())
 ## Set working directory
-setwd("C:/Users/Adam/Documents/UC Berkeley post doc/BIGCB/Pest Project")
+#setwd("C:/Users/Adam/Documents/UC Berkeley post doc/BIGCB/Pest Project")
 
 ## Load libraries
 my_packages<-c('rgdal', 'sp', 'maptools', 'SDMTools', 'rgeos',
@@ -10,39 +10,37 @@ my_packages<-c('rgdal', 'sp', 'maptools', 'SDMTools', 'rgeos',
 lapply(my_packages, require, character.only=T)
 
 ## Load functions and polygons
-source("C:/Users/Adam/Documents/UC Berkeley post doc/BIGCB/Pest Project/Occupancy modeling/Occupancy_functions_and_polygons.R")
+source("R_functions/museum_specimen_analysis_functions.R")
 
 #### Reference raster for California
 #### Using BCM 2014 raster for Minimum temperature for 11/2000
 #### Resolution of BCM 2014 data = 270 m x 270 m
 #### Rdata file loaded is a list with raster as 1st item, metadata as 2nd item
-Ref_raster_data <- readRDS("Climate data/BCM2014/Rasters/tmn/CA_BCM_HST_Monthly_tmn_2000_11.Rdata")
+Ref_raster_data <- readRDS("C:/Users/Adam/Documents/UC Berkeley post doc/BIGCB/Pest ProjectClimate data/BCM2014/Rasters/tmn/CA_BCM_HST_Monthly_tmn_2000_11.Rdata")
 Ref_raster <- Ref_raster_data[[1]]
-Ref_raster <- aggregate(Ref_raster, 37)
+nkm <- 15 # Cell dimension, as number of km on a side
+# To get cells 10 km x 10 km, aggregate by 37
+ncells <- round(3.703704*nkm, digits = 0) # Number of cells to aggregate into 1 new cell to get desired cell size
+Ref_raster <- aggregate(Ref_raster, ncells) 
+# Need to redefine extent of BCM raster in terms of lat and long 
 dem <- raster("Climate data/ca_270m_t6.asc")
 extent(Ref_raster) <- extent(dem)
-## Plotting reference raster
-# Reference raster for California, Nevada, Arizona, Utah
-# Original raster cells are 5 degree minutes on each side
-# CAsp <- raster("Potato psyllid/Potato Psyllid Data/MaxTemp_1895.gri") # Alternative reference raster
-# Ref_raster <- aggregate(CAsp, 5)
-# Aggregated at "15", raster cells are 75 degree minutes, or 1.25 degrees
 
 #### Load data
 # Species records
-hemipRecords <- readRDS("Potato psyllid/Potato psyllid data/Compiled_Hemiptera_records_2016-01-19.rds")
+hemipRecords <- readRDS("output/Compiled_Hemiptera_records_2016-01-19.rds")
 
-#### Check for records of various species from presence-absenced data sets
-rglpsyllids <- hemipRecords[hemipRecords$ScientificName == "Glycaspis brimblecombei",] # Red gum lerp psyllid; 39 records
-gss <- hemipRecords[hemipRecords$ScientificName == "Draeculacephala minerva",] # green sharpshooter; 267 records
-rss <- hemipRecords[grep("Carneocephala", hemipRecords$ScientificName),] # red-headed sharpshooter; 0 records
-bgss <- hemipRecords[hemipRecords$ScientificName == "Graphocephala atropunctata",] # blue-green sharpshooter; 96 records 
-hb <- hemipRecords[hemipRecords$ScientificName == "Murgantia histrionica",] # harlequin bug; 360 records
-blackscale <- hemipRecords[hemipRecords$ScientificName == "Saissetia oleae",] # black scale; 93 records
-redscale <- hemipRecords[hemipRecords$ScientificName == "Aonidiella aurantii",]
-# Remove carnivorous and aquatic Heteropteran families
-table(hemipRecords$Family)
-write.csv(unique(hemipRecords$Family), file = "Potato psyllid/Potato psyllid data/Collected_Hemiptera_Families.csv", row.names = FALSE)
+# #### Check for records of various species from presence-absenced data sets
+# rglpsyllids <- hemipRecords[hemipRecords$ScientificName == "Glycaspis brimblecombei",] # Red gum lerp psyllid; 39 records
+# gss <- hemipRecords[hemipRecords$ScientificName == "Draeculacephala minerva",] # green sharpshooter; 267 records
+# rss <- hemipRecords[grep("Carneocephala", hemipRecords$ScientificName),] # red-headed sharpshooter; 0 records
+# bgss <- hemipRecords[hemipRecords$ScientificName == "Graphocephala atropunctata",] # blue-green sharpshooter; 96 records 
+# hb <- hemipRecords[hemipRecords$ScientificName == "Murgantia histrionica",] # harlequin bug; 360 records
+# blackscale <- hemipRecords[hemipRecords$ScientificName == "Saissetia oleae",] # black scale; 93 records
+# redscale <- hemipRecords[hemipRecords$ScientificName == "Aonidiella aurantii",]
+# # Remove carnivorous and aquatic Heteropteran families
+# table(hemipRecords$Family)
+# write.csv(unique(hemipRecords$Family), file = "Potato psyllid/Potato psyllid data/Collected_Hemiptera_Families.csv", row.names = FALSE)
 
 # List of non-herbivorous families (predaceous, paristic, and aquatic)
 noherbFamilies <- c("Reduviidae", "Geocoridae", "Nabidae", "Cimicidae", "Pieidae",
@@ -62,6 +60,12 @@ RawRecords$Locality <- as.character(RawRecords$Locality)
 #RawRecords$Source <- as.character(RawRecords$Source)
 RawRecords$Collector <- as.character(RawRecords$Collector)
 RawRecords$Error <- as.numeric(as.character(RawRecords$MaxErrorInMeters))
+
+# # Select only collectors of potato psyllids
+# ppCollectors <- unique(RawRecords[RawRecords$ScientificName == "Bactericera cockerelli", "Collector"])
+# ppCollectors <- ppCollectors[!ppCollectors == ""]
+# ppCollectors <- ppCollectors[!ppCollectors == "unknown"]
+# saveRDS(ppCollectors, "Potato psyllid/Potato psyllid data/potato_psyllid_collectors.rds")
 
 ##### DATA PROCESSING #####
 ## Process species records
@@ -88,14 +92,14 @@ Records$cellID <- factor(cellFromXY(Ref_raster, RecordSP)) # Note: this line of 
 Records$eventID <- apply(Records[c("YearCollected", "cellID")], 1, function(x) paste(x[1], x[2], sep = ", "))
 # Add a collection ID (i.e. event*MonthCollected)
 Records$collectionID <- apply(Records[c("eventID", "MonthCollected")], 1, function(x) paste(x[1], x[2], sep = ", "))
-# Remove duplicate observations that include identical occupancy information 
-Records <- Records[!duplicated(Records[c("Species", "collectionID")]),]
 # Select only records after 1900
 Records <- subset(Records, YearCollected >= 1900 & YearCollected < 2015)
 # Subset to only California
 Records <- Records[Records$StateProvince == "California",]
 # Remove NAs
 Records <- Records[which(!is.na(Records$cellID) & !is.na(Records$Species)),]
+# Remove duplicate observations that include identical occupancy information 
+Records <- Records[!duplicated(Records[c("Species", "collectionID")]),]
 # Remove bad species names: ones without epithet or "undetermined"
 speciesNames <- unique(Records$ScientificName)
 badNames <- c(speciesNames[grep(" sp.", speciesNames, fixed = TRUE)],
@@ -111,14 +115,21 @@ Records <- Records[which(Records$ScientificName %in% goodNames),]
 ## Climate extraction functions
 source("Climate data/Extract_climate_data_functions.R")
 cdir <- "C:/Users/Adam/Documents/UC Berkeley post doc/BIGCB/Pest Project/Climate data/BCM2014/Rasters"
+
+# Note: For annual Tmin and Tmax, I'm using water year summaries.
+# I should extract the Tmin and Tmax of the previous water year from the year the collection was made.
+# This ensures that the temperature occurred prior to when the insects were collected.
+# Make a column of t-1 where t = YearCollected
+Records$YearCollectedm1 <- Records$YearCollected - 1
+Records <- subset(Records, YearCollectedm1 >= 1900)
 # First, extract monthly aet and cwd data
 Records <- Records %>% extractClimateMonthly(., colNames = c("DecimalLongitude", "DecimalLatitude", "YearCollected", "MonthCollected"),
                                              fac = c("aet", "cwd"), cdir = cdir) %>%
-                   # Second, extract annual min summary of tmn data
-                   extractClimateAnnually(., colNames = c("DecimalLongitude", "DecimalLatitude", "YearCollected"),
+                   # Second, extract annual min summary of tmn data, for year previous to YearCollected
+                   extractClimateAnnually(., colNames = c("DecimalLongitude", "DecimalLatitude", "YearCollectedm1"),
                                           fac = c("tmn"), summaryType = "min", cdir = cdir) %>%
-                   # Third, extract annual max summary of tmx data
-                   extractClimateAnnually(., colNames = c("DecimalLongitude", "DecimalLatitude", "YearCollected"),
+                   # Third, extract annual max summary of tmx data, for year previous to YearCollected
+                   extractClimateAnnually(., colNames = c("DecimalLongitude", "DecimalLatitude", "YearCollectedm1"),
                                           fac = c("tmx"), summaryType = "max", cdir = cdir)
 
 
@@ -126,10 +137,10 @@ Records <- Records %>% extractClimateMonthly(., colNames = c("DecimalLongitude",
 ## Make species lists from Essig, GBIF, and CDFA datasets
 ## Two lists for Fithian's Proportional Bias model:
 ## PALists = Presence-absence lists; POLists = presence-only lists
-PALists <- make_lists(Records, 4)
+#PALists <- make_lists(Records, 4)
 AllLists <- make_lists(Records, 1)
 speciesNames <- unique(Records$Species)
-saveRDS(AllLists, file = "Potato psyllid/Potato psyllid data/All_Hemip_Lists_Climate_2016-02-18.rds")
+saveRDS(AllLists, file = "output/All_Hemip_Lists_Climate_15km_Cells_2016-03-25.rds")
 
 
 ##############################################################################################
@@ -138,7 +149,7 @@ saveRDS(AllLists, file = "Potato psyllid/Potato psyllid data/All_Hemip_Lists_Cli
 listData <- as.data.frame(rbindlist(AllLists))
 listData[listData$Species == "Bactericera.cockerelli",]
 nrow(listData[listData$Species == "Bactericera.cockerelli",])
-saveRDS(listData, "Potato psyllid/Potato psyllid data/All_Hemip_species_lists_2016-02-18.rds")
+saveRDS(listData, "output/All_Hemip_species_lists_2016-02-18.rds")
 
 # How are the list lengths distributed?
 ListLength <- as.numeric(unlist(lapply(PALists, function(x) nrow(x))))
@@ -146,7 +157,7 @@ hist(ListLength, breaks = seq(4,40,1))
 
 # Where are the lists distributed?
 xyLists <- data.frame(xyFromCell(Ref_raster, cell = as.numeric(levels(listData$cellID))[listData$cellID]))
-tiff("Potato psyllid/Potato psyllid analyses/Figures/pp_list_map.tif")
+tiff("results/figures/pp_list_map.tif")
   map("state", regions = c("california"))
   points(xyLists$x, xyLists$y, pch = 1, col = "grey35", cex = 1.8)
   points(listData[listData$Species == "Bactericera.cockerelli",]$DecimalLongitude,
@@ -168,28 +179,28 @@ listdate <- listdate[!duplicated(listdate[c("Year", "Season")]),]
 ## Make figures for lab meeting
 ppRecords <- SArecords[SArecords$ScientificName == "Bactericera cockerelli" & SArecords$StateProvince == "California",]
 # map of all Stern + Auchen records
-tiff("Potato psyllid/Potato psyllid analyses/Figures/all_potato_psyllid_records_CAmap.tif")
+tiff("results/figures/all_potato_psyllid_records_CAmap.tif")
   map("state", regions = c("california"))
   points(ppRecords$DecimalLongitude,
          ppRecords$DecimalLatitude,
          pch = 1, col = "darkgreen", cex = 1.8)
 dev.off()
 
-tiff("Potato psyllid/Potato psyllid analyses/Figures/potato_psyllid_histogram.tif")
+tiff("results/figures/potato_psyllid_histogram.tif")
   hist(ppRecords$YearCollected, ylab = "Frequency", xlab = "Year Collected",
        main = "", xlim = c(1900,2020))
 dev.off()
 
 # map of all Stern + Auchen records
 CArecords <- SArecords[SArecords$StateProvince == "California",]
-tiff("Potato psyllid/Potato psyllid analyses/Figures/all_SA_records_CAmap.tif")
+tiff("results/figures/all_SA_records_CAmap.tif")
   map("state", regions = c("california"))
   points(CArecords$DecimalLongitude,
          CArecords$DecimalLatitude,
          pch = 1, col = "darkgreen", cex = 1.8)
 dev.off()
 
-tiff("Potato psyllid/Potato psyllid analyses/Figures/SA_histogram.tif")
+tiff("results/figures/SA_histogram.tif")
   hist(CArecords$YearCollected, ylab = "Frequency", xlab = "Year Collected",
        main = "", xlim = c(1900,2020))
 dev.off()
@@ -201,7 +212,7 @@ CAraster <- crop(Ref_raster, CAcrop)
 plot(CAraster)
 CApoly <- rasterToPolygons(CAraster) 
 listpp <- listData[listData$ScientificName == "Bactericera cockerelli",]
-tiff("Potato psyllid/Potato psyllid analyses/Figures/CA_polygon.tif")
+tiff("results/figures/CA_polygon.tif")
   map("state", regions="california")
   lines(CApoly, lty = 2, col = "darkgrey")
   points(listpp$DecimalLongitude,
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # potato_psyllid_distribution_modeling
-Analysis of potato psyllid museum specimens
-Including analysis using GLMM and maybe other models
+Analysis of potato psyllid museum specimens.
+Including analysis using binomial GLMM, Bayesian GLMM, and zero-inflated binomial GLMM.
+Subfolder "R_analyses" includes scripts specifying models.
+"R_functions" includes functions called in other scripts.
+"output" includes data sets for analysis and model parameter estimates.
