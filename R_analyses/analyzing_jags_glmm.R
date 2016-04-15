7beb11d4be36d4ad8d935138ae38c2f7f0f1955c
arzeilinger <arzeilinger@gmail.com>
2016-04-15 00:48:28 -0700

    Made more general function for predicting occupancy from model

--- R_analyses/analyzing_jags_glmm.R
+++ R_analyses/analyzing_jags_glmm.R
@@ -5,6 +5,8 @@ rm(list = ls())
 my_packages<-c('data.table', 'lattice', 'akima', 'tidyr', 'RCurl', 'foreign')
 lapply(my_packages, require, character.only=T)
 
+source("R_functions/museum_specimen_analysis_functions.R")
+
 ## Load JAGS model coefficient estimates from GitHub
 url <- "https://raw.githubusercontent.com/arzeilinger/potato_psyllid_distribution_modeling/master/GLM/climate_glmm_params.csv"
 glmmResults <- getURL(url) %>% textConnection() %>% read.csv(., header = TRUE)
@@ -13,7 +15,7 @@ glmmResults[grep("beta", glmmResults$params),]
 #### Analyzing ZIB GLMM model output
 
 ## Load JAGS coefficient estimates from local folder
-glmmResults <- readRDS("output/zib_glmm_jags_out_params.rds")
+glmmResults <- readRDS("output/climate_glmm_jags_out_params.rds")
 glmmResults <- glmmResults[,c("mean", "X2.5.", "X97.5.", "r.hat")]
 names(glmmResults) <- c("mean", "cil", "ciu", "rhat")
 glmmResults$params <- row.names(glmmResults)
@@ -24,7 +26,7 @@ glmmResults[grep("beta", glmmResults$params),]
 url <- "https://raw.githubusercontent.com/arzeilinger/potato_psyllid_distribution_modeling/master/output/potato_psyllid_detection_dataset.csv"
 detectData <- getURL(url) %>% textConnection() %>% read.csv(., header = TRUE)
 ## Load detection dataset from local folder
-#detectData <- readRDS("potato_psyllid_detection_dataset.rds")
+detectData <- readRDS("output/potato_psyllid_detection_dataset.rds")
 str(detectData)
 
 ##################################################################################
@@ -44,36 +46,30 @@ for(i in 1:length(siteRE$cellID)){
 detectData <- cbind(detectData, siteAlpha)
 
 ## Model coefficients
-betas <- glmmResults[grep("betap", glmmResults$params), "mean"]
+betas <- glmmResults[grep("beta", glmmResults$params), "mean"]
 
-## Function to predict potato psyllid occupancy from model results
-# includes site random effects
-predFunc <- function(x){
-  yv <- plogis(betas[1]*x[1] + betas[2]*x[1] + # quadriatic year
-                 betas[3]*x[2] + betas[4]*x[2]^2 + # quadratic month covariates
-                 #betas[4]*x[3] + # list length
-                 betas[5]*x[3] + betas[6]*x[4] + betas[7]*x[5] + betas[8]*x[6] + # climate covariates
-                 x[7]) # site random effect (alpha)
-  return(yv)
-}
+#### Predictions from full binomial model
+# Setup all covariates, including quadratic and interaction
+detectData$month2 <- detectData$stdmonth^2
+detectData$llyr <- detectData$stdlnlist_length*detectData$stdyear
+covars <- c("stdyear", "stdmonth", "month2", "stdlnlist_length", "llyr", "stdaet", "stdtmn", "stdtmx", "siteAlpha")
+detectData$predOcc <- predFunc(betas = betas, covars = covars)
 
-# Use standardized old data to get predicted occupancy
-stdxmat <- detectData[,c("stdyear", "stdmonth", "stdaet", "stdcwd", "stdtmn", "stdtmx", "siteAlpha")]
-# Predicted occupancy
-detectData$predocc <- apply(stdxmat, 1, predFunc)
 
 # plots
 plot(x = detectData$year, y = detectData$detection)
-lines(smooth.spline(detectData$year, detectData$predocc))
+lines(smooth.spline(detectData$year, detectData$predOcc))
 
-plot(x = detectData$aet, y = detectData$predocc)
-plot(x = detectData$tmn, y = detectData$predocc)
-plot(x = detectData$month, y = detectData$predocc)
-lines(smooth.spline(detectData$month, detectData$predocc))
-plot(x = detectData$lnlist_length, y = detectData$predocc)
+plot(x = detectData$aet, y = detectData$predOcc)
+plot(x = detectData$tmn, y = detectData$predOcc)
+lines(smooth.spline(detectData$tmn, detectData$predOcc))
+
+plot(x = detectData$month, y = detectData$predOcc)
+lines(smooth.spline(detectData$month, detectData$predOcc))
+plot(x = detectData$lnlist_length, y = detectData$predOcc)
 
 # trivariate plots with month and year
-zz <- with(detectData, interp(x = year, y = month, z = predocc, duplicate = 'median'))
+zz <- with(detectData, interp(x = year, y = month, z = predOcc, duplicate = 'median'))
 pdf("results/figures/year-month-occupancy_contourplot2.pdf")
   filled.contour(zz, col = topo.colors(32), xlab = "Year", ylab = "Month")
 dev.off()
