4ba8ae2dd34b94e9e6c8757597c4b4df76e8a5f0
arzeilinger <arzeilinger@gmail.com>
2016-04-14 18:24:00 -0700

    Updated model set for glmer. Full model coverges.

--- R_analyses/glmm_list_length_analysis.R
+++ R_analyses/glmm_list_length_analysis.R
@@ -9,36 +9,8 @@ lapply(my_packages, require, character.only=T)
 ## Load functions
 source("R_functions/museum_specimen_analysis_functions.R")
 
-# Load species lists data set with climate data 
-AllLists <- readRDS("Potato psyllid data/All_Hemip_Lists_Climate_15km_Cells_2016-03-25.rds")
-
-# Collectors of potato psyllids, from RawRecords data set in making_species_lists
-ppCollectors <- readRDS("Potato psyllid data/potato_psyllid_collectors.rds")
-
-# Keep only long lists (ll > 3)
-longLists <- AllLists %>% rbindlist() %>% as.data.frame() %>% make_lists(., min.list.length = 3)
-longListsDF <- longLists %>% rbindlist() %>% as.data.frame()
-
-# select only lists that contain collectors of potato psyllids
-ppcCollections <- longListsDF[longListsDF$Collector %in% ppCollectors, "collectionID"]
-ppcData <- longListsDF[longListsDF$collectionID %in% ppcCollections,]
-ppcLists <- ppcData %>% make_lists(., min.list.length = 3)
-
-# Transform to data frame with pp detection
-detectData <- detectDataFunc(ppcLists) 
-detectData <- dplyr::filter(detectData, !is.na(aet) & !is.na(cwd) & !is.na(tmn) & !is.na(tmx))
-detectData$lnlist_length <- log(detectData$list_length)
-
-# standardize numeric covariates, include as new variables in data frame
-covars <- c("year", "month", "lnlist_length", "aet", "cwd", "tmn", "tmx")
-covars.i <- as.numeric(sapply(covars, function(x) which(names(detectData) == x), simplify = TRUE))
-for(i in covars.i){
-  var.i <- names(detectData)[i]
-  stdname.i <- paste("std", var.i, sep = "")
-  stdvar.i <- standardize(detectData[,var.i])
-  detectData[,stdname.i] <- stdvar.i
-}
-
+## Load detection data set, set up from making_jags_glmm_data.R script
+detectData <- readRDS("output/potato_psyllid_detection_dataset.rds")
 
 # Exploring data
 table(detectData$detection)
@@ -56,52 +28,42 @@ detectData %>% group_by(cellID) %>% summarise(nvisits = length(cellID))
 ## Using previously standardized covariates and full data set
 
 # Full model
-glmModFull <- glmer(detection ~ stdlnlist_length + stdyear + stdmonth + I(stdmonth^2) + 
-                    stdaet + stdcwd + stdtmn + stdtmx + (1|cellID), 
+glmModFull <- glmer(detection ~ stdlnlist_length + stdyear + stdlnlist_length*stdyear + 
+                      stdmonth + I(stdmonth^2) + 
+                    stdaet + stdtmn + stdtmx + (1|cellID), 
                   family = "binomial", data = detectData,
                   #control = glmerControl(optCtrl = list(method = "spg", ftol = 1e-20, maxit = 100000)))
                   control = glmerControl(optimizer = "bobyqa"))
 summary(glmModFull)
-# Model with season instead of month (season is a factor)
-glmModSeason <- glmer(detection ~ stdlnlist_length + season*stdyear + 
-                    stdaet + stdcwd + season*stdtmn + season*stdtmx + (1|cellID), 
-                  family = "binomial", data = detectData,
-                  #control = glmerControl(optCtrl = list(method = "spg", ftol = 1e-20, maxit = 100000)))
-                  control = glmerControl(optimizer = "bobyqa"))
 # Model without month
-glmModLL2 <- glmer(detection ~ stdlnlist_length + stdyear +  
-                    stdaet + stdcwd + stdtmn + stdtmx + (1|cellID), 
+glmModLL2 <- glmer(detection ~ stdlnlist_length + stdyear + stdlnlist_length*stdyear + 
+                    stdaet + stdtmn + stdtmx + (1|cellID), 
                   family = "binomial", data = detectData,
                   #control = glmerControl(optCtrl = list(method = "spg", ftol = 1e-20, maxit = 100000)))
                   control = glmerControl(optimizer = "bobyqa"))
 # Model with only tmn and tmx
-glmModLL3 <- glmer(detection ~ stdlnlist_length + stdyear + 
+glmModLL3 <- glmer(detection ~ stdlnlist_length + stdyear + stdlnlist_length*stdyear + 
                     stdtmn + stdtmx + (1|cellID), 
                   family = "binomial", data = detectData,
                   #control = glmerControl(optCtrl = list(method = "spg", ftol = 1e-20, maxit = 100000)))
                   control = glmerControl(optimizer = "bobyqa"))
 # Model with only climate variables
-glmModLL4 <- glmer(detection ~ stdlnlist_length +  
-                    stdaet + stdcwd + stdtmn + stdtmx + (1|cellID), 
+glmModLL4 <- glmer(detection ~ stdlnlist_length +
+                    stdaet + stdtmn + stdtmx + (1|cellID), 
                   family = "binomial", data = detectData,
                   #control = glmerControl(optCtrl = list(method = "spg", ftol = 1e-20, maxit = 100000)))
                   control = glmerControl(optimizer = "bobyqa"))
 # Model with only year and LL
-glmModLL5 <- glmer(detection ~ stdlnlist_length + stdyear +
+glmModLL5 <- glmer(detection ~ stdlnlist_length + stdyear + stdlnlist_length*stdyear + 
                     (1|cellID), 
                   family = "binomial", data = detectData,
-                  #control = glmerControl(optCtrl = list(method = "spg", ftol = 1e-20, maxit = 100000)))
-                  control = glmerControl(optimizer = "bobyqa"))
-# Model with year*month interaction
-glmModLL6 <- glmer(detection ~ stdlnlist_length + stdyear*stdmonth +
-                     standardize(year)*I(standardize(month)^2) +
-                    (1|cellID), 
-                  family = "binomial", data = detectData,
-                  #control = glmerControl(optCtrl = list(method = "spg", ftol = 1e-20, maxit = 100000)))
-                  control = glmerControl(optimizer = "bobyqa"))
-AICtab(glmModFull, glmModSeason, glmModLL2, glmModLL3, glmModLL4, glmModLL5, glmModLL6, base = TRUE)
-# glmModLL2 (full model without month terms) seems to be best
-summary(glmModLL2)
+                  control = glmerControl(optCtrl = list(method = "spg", ftol = 1e-20, maxit = 100000)))
+                  #control = glmerControl(optimizer = "bobyqa"))
+
+AICtab(glmModFull, glmModLL2, glmModLL3, glmModLL4, glmModLL5, base = TRUE)
+# glmModLL5 has lowest AIC but doesn't converge well
+# glmModFull performs nearly as well as converges successfully
+summary(glmModFull)
 
 
 ###########################################################################################################
@@ -169,12 +131,14 @@ extractModelRuns <- lapply(runs, modelCoefFunc)
 modelSummaries <- lapply(extractModelRuns, summary)
 
 
+########################################################################################################################
 #### Model predictions
 # Using one subsample iteration of best model
-bestModel <- extractModelRuns[[1]]
+bestModel <- glmModFull
 detectData$predOcc <- predict(bestModel, type = "response", re.form = NA)
 
 plot(x = detectData$year, y = detectData$predOcc)
 plot(x = detectData$lnlist_length, y = detectData$predOcc)
 plot(x = detectData$tmn, y = detectData$predOcc)
 plot(x = detectData$aet, y = detectData$predOcc)
+plot(x = detectData$month, y = detectData$predOcc)
